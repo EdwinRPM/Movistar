776ead36ef4f6eaae955b781b7b6158300f005fe
EdwinRPM <80488409+EdwinRPM@users.noreply.github.com>
2021-04-03 12:28:57 -0500

    Add files via upload

---
+++ JS/Dibujadigura.js
@@ -0,0 +1,8 @@
+function dibujafiguras(){
+
+		var canvas = document.getElementById('miCanvas'):
+		var contexto= canvas.getContex('2d');
+		contexto.fillStytle='red';
+		contexto.fillrect(50,50,150,200);
+
+}
